60dc432ffbc72ee1eb99bfba081a258f6edf83d7
rradkris <r.radkris@gmail.com>
2016-05-10 10:06:29 +0100

    Add files via upload
    Test cases added for deals

--- ShoppingCart/src/test/java/shopping/cart/service/test/ShoppingCartTest.java
+++ ShoppingCart/src/test/java/shopping/cart/service/test/ShoppingCartTest.java
@@ -36,6 +36,57 @@ public class ShoppingCartTest {
         Assert.assertEquals(new Double(1.7), checkOutService.getPrice());
     }
 
+    @Test
+    public void verifyAppleOffer() {
+        //ASSIGN
+        repeat(Constants.MAGIC_NUMBER_FIVE, () -> {
+            cart.addProduct(new Apple(Constants.APPLE_PRODUCT_CODE, Product.Type.APPLE.name(), Constants.APPLE_PRODUCT_PRICE, new Deal(Constants.APPLE_PRODUCT_CODE, Constants.MAGIC_NUMBER_TWO)));
+        });
+
+        //ACT
+        CheckoutService checkOutService = new CheckoutService();
+        checkOutService.checkout(cart);
+
+        //ASSERT
+        Assert.assertEquals(new Double(1.8), checkOutService.getPrice());
+    }
+
+    @Test
+    public void verifyOrangeDeal() {
+        //ASSIGN
+        repeat(Constants.MAGIC_NUMBER_SEVEN, () -> {
+            cart.addProduct(new Orange(Constants.ORANGE_PRODUCT_CODE, Product.Type.ORANGE.name(), Constants.ORANGE_PRODUCT_PRICE, new Deal(Constants.ORANGE_PRODUCT_CODE, Constants.MAGIC_NUMBER_THREE)));
+        });
+
+        //ACT
+        CheckoutService checkOutService = new CheckoutService();
+        checkOutService.checkout(cart);
+
+        //ASSERT
+        Assert.assertEquals(new Double(1.25), checkOutService.getPrice());
+
+        //Delete a product
+        cart.deleteProduct(0);
+        checkOutService.checkout(cart);
+        Assert.assertEquals(new Double(1.00), checkOutService.getPrice());
+    }
+
+    @Test
+    public void verifyDeals() {
+        //ASSIGN
+        repeat(Constants.MAGIC_NUMBER_NINE, () -> {
+            cart.addProduct(new Orange(Constants.ORANGE_PRODUCT_CODE, Product.Type.ORANGE.name(), Constants.ORANGE_PRODUCT_PRICE, new Deal(Constants.ORANGE_PRODUCT_CODE, Constants.MAGIC_NUMBER_THREE)));
+            cart.addProduct(new Apple(Constants.APPLE_PRODUCT_CODE, Product.Type.APPLE.name(), Constants.APPLE_PRODUCT_PRICE, new Deal(Constants.APPLE_PRODUCT_CODE, Constants.MAGIC_NUMBER_TWO)));
+        });
+
+        //ACT
+        CheckoutService checkOutService = new CheckoutService();
+        checkOutService.checkout(cart);
+
+        //ASSERT
+        Assert.assertEquals(new Double(4.5), checkOutService.getPrice());
+    }
+
     @After
     public void tearDown() {
         cart = null;
